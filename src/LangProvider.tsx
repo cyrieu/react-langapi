e1bee0fa838ab5101dcb4682ecfac84e8c93a14c
cyrieu <cyrieu@gmail.com>
2019-06-20 19:25:17 -0700

    Use both for backwards compatibility

--- src/LangProvider.tsx
+++ src/LangProvider.tsx
@@ -4,15 +4,22 @@ import { LangContext } from "./Context";
 export type LangProviderProps = {
   currentLanguage?: string;
   client: any;
+  langClient: any;
 };
 
 class LangProvider extends React.Component<LangProviderProps> {
   render() {
-    const { currentLanguage, client } = this.props;
+    const { currentLanguage, client, langClient } = this.props;
     // TODO THIS DOES NOT WORK IF CHILDREN IS A NORMAL DOM NODE
-    client.setForceLanguage(currentLanguage);
+    const chosenClient = client != null ? client : langClient;
+    chosenClient.setForceLanguage(currentLanguage);
     return LangContext ? (
-      <LangContext.Provider value={{ currentLanguage, client }}>
+      <LangContext.Provider
+        value={{
+          currentLanguage,
+          client: chosenClient
+        }}
+      >
         {this.props.children}
       </LangContext.Provider>
     ) : (
