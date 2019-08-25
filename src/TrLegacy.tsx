73d1ffccb2da5638b65f0a01e813581fd6f13af5
Abhinand Sivaprasad <abhi@live.com>
2019-08-25 13:24:15 -0700

    moved to legacy Tr component

---
+++ src/TrLegacy.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { renderNodes } from "./reactUtils";
+import { TrOptions } from "./types";
+import {LangTranslateClient} from "langapi-client/dist/LangTranslateClient";
+
+type TrProps = {
+    client: LangTranslateClient
+    description?: string;
+    variables?: any;
+    options?: TrOptions;
+};
+
+type State = {};
+
+class TrLegacy extends React.Component<TrProps, State> {
+    render() {
+        const {
+            children,
+            description,
+            variables,
+            options,
+            client,
+            ...rest
+        } = this.props;
+        // fallback to original elements
+        const fallback = children ? <div>{children}</div> : false;
+
+        // translated react nodes
+        const transformedNodes = renderNodes(client, children, variables, options);
+
+        return transformedNodes ? transformedNodes : fallback;
+    }
+}
+
+export default TrLegacy;
